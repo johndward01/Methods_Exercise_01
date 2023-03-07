0e24223732f55daa5f708a8600675b0ee75ca3ca
johndward01 <johndward01@gmail.com>
2023-03-07 07:37:52 -0600

    Add project files.

---
+++ Methods_Exercise_01/Dog.cs
@@ -0,0 +1,6 @@
+﻿namespace Methods_Exercise_01;
+internal class Dog
+{
+    public int Age { get; set; }
+    public string Name { get; set; }
+}
---
+++ Methods_Exercise_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using Methods_Exercise_01;
+
+// DECLARE ALL YOUR METHODS INSIDE OF THE UTILITY CLASS AND CALL THEM HERE!
+
+
+
+
+
+
+
+#region Bonus (Open this if you are completing the bonus) 
+var dogs = new List<Dog>()
+{
+    new Dog(){Age = 7, Name = "Fido"},
+    new Dog(){Age = 2, Name = "Spot"}, 
+    new Dog(){Age = 3, Name = "Snoopy"},
+    new Dog(){Age = 4, Name = "Scooby"},
+    new Dog(){Age = 5, Name = "Rin Tin Tin"},
+    new Dog(){Age = 6, Name = "Old Yeller"}
+};
+
+#endregion

62a7702c49c649d8c41af803b992a9fcc95574cc
johndward01 <johndward01@gmail.com>
2023-03-07 08:39:05 -0600

    updated

--- Methods_Exercise_01/Program.cs
+++ Methods_Exercise_01/Program.cs
@@ -3,20 +3,69 @@
 // DECLARE ALL YOUR METHODS INSIDE OF THE UTILITY CLASS AND CALL THEM HERE!
 
 
+// TODO: Use this variable for TODO #1
+var first = 7;
 
 
+// TODO: Use this variable for TODO #2
+var second = 7;
 
 
+// TODO: Use this variable for TODO #3
+var third = "Thomas Smith";
 
-#region Bonus (Open this if you are completing the bonus) 
-var dogs = new List<Dog>()
+
+// TODO: Use these variables for TODO #4
+var value1 = 135;
+var value2 = 15;
+
+
+// TODO: Use these variables for TODO #5
+var value3 = 150.00;
+var value4 = 11.99;
+var value5 = 1.10;
+
+
+// TODO: Use this variable for TODO #6
+var reallyStrangeString = "alsdkfjsadlkeqwerrtyuyuiopz,xc.vn.,mncv,./cmb!&*!^%#@*@#$^)(#*%&)(#&%]pasdflfdkjgtwe";
+
+
+// TODO: Use this variable for TODO #7
+var anArrayOfStrings = new[]
 {
-    new Dog(){Age = 7, Name = "Fido"},
-    new Dog(){Age = 2, Name = "Spot"}, 
-    new Dog(){Age = 3, Name = "Snoopy"},
-    new Dog(){Age = 4, Name = "Scooby"},
-    new Dog(){Age = 5, Name = "Rin Tin Tin"},
-    new Dog(){Age = 6, Name = "Old Yeller"}
+    "The",
+    "quick",
+    "brown",
+    "fox",
+    "super long string!!!",
+    "jumps",
+    "over",
+    "the",
+    "lazy",
+    "dog"
 };
 
-#endregion
+
+// TODO: Use this variable for TODO #7
+var aListOfIntegers = new List<int>()
+{
+    23343444,
+    2653437,
+    2232123,
+    2465327,
+    241433546,
+    133334243,
+    3543563
+};
+
+
+// TODO: Use this list of dogs for the BONUS
+var dogs = new List<Dog>()
+{
+    new Dog(){Age = 4, Name = "Scooby"},
+    new Dog(){Age = 2, Name = "Spot"},
+    new Dog(){Age = 7, Name = "Fido"},
+    new Dog(){Age = 3, Name = "Snoopy"},
+    new Dog(){Age = 6, Name = "Old Yeller"},
+    new Dog(){Age = 5, Name = "Rin Tin Tin"},
+};
--- Methods_Exercise_01/Utility.cs
+++ Methods_Exercise_01/Utility.cs
@@ -1,30 +1,31 @@
 ﻿namespace Methods_Exercise_01;
-internal class Utility
+internal static class Utility
 {
     // TODO: Create a Method that accepts a number and returns true if it is even
-     
+    
 
     // TODO: Create a Method that accepts a number and returns true if it is odd
-
+    
 
     // TODO: Create a Method that accepts a string (person's name) and returns a greeting
-
+    
 
     // TODO: Create a method that accepts 2 integers and adds them 
-
+    
 
     // TODO: Create a method that accepts 3 doubles and subtracts them
-
+    
 
     // TODO: Create a method that takes a string and returns the last character of that string
-
+    
 
     // TODO: Create a method that takes an array of strings and returns the string that has the greatest length
-
+    
 
     // TODO: Create a method that takes a list of integers and returns the smallest number
+    
 
-
-    // BONUS: Create a method that takes a list of dogs and returns the dogs name who is the oldest (the dog class and list of dogs has already been created)
+    // BONUS: Create a method that takes a list of dogs and returns the name of the dog who is the oldest (the dog class and list of dogs has already been created)
+    
 
 }
